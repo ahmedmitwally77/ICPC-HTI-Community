ee3e0e77a37c5f7118ce063393bb15d9c474be76
ziad essa <helmyzez@gmail.com>
2024-11-18 03:13:35 +0200

    Add Notes

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import { Outlet } from "react-router-dom";
 import Footer from './Footer/Footer';
